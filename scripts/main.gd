25b31a5ab8d094dfd4fa06e7624aeea41e66fc66
Gabriel B.F <galabyca@gmail.com>
2024-04-13 08:30:08 -0400

    added GameManager singleton + branch

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Start.connect("pressed", _on_Start_pressed)
 	$Start.grab_focus()
 
 func _on_Start_pressed():
-	get_tree().change_scene_to_file("res://scenes/restaurant.tscn")
+	# Reset or initialize game settings
+	GameManager.start_game()
+	get_tree().change_scene_to("res://scenes/restaurant.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
