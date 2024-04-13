cd50a178e7caf9e4250637895854ae5e5ff4dc4b
Gabriel B.F <galabyca@gmail.com>
2024-04-13 14:49:37 -0400

    Customer logic

--- scripts/customer.gd
+++ scripts/customer.gd
@@ -1,3 +1,4 @@
+# Customer.gd
 extends Area2D
 
 # Member variables
@@ -5,21 +6,30 @@ var order = null
 var patience = 100.0
 
 func _ready():
-	# Initialize any necessary variables or states here
 	pass
 
-# Decrease patience over time or due to other triggers
 func decrease_patience(delta):
 	patience -= delta
 	if patience <= 0:
 		leave()
 
-# Placeholder for customer departure
 func leave():
 	print("Customer is leaving due to impatience or order fulfillment.")
 	queue_free()  # This will remove the Customer instance from the scene
 
-# Placeholder to set an order, can be expanded later
 func set_order(new_order):
 	order = new_order
-	# Update customer's thought bubble or similar UI element here
+	print("Order set to: " + order)
+	# Future implementation: Update customer's thought bubble or similar UI element here
+
+func receive_order(meal):
+	if meal == null:
+		print("No meal provided to the customer.")
+		return false
+	elif meal != order:
+		print("Wrong meal provided. Customer wanted: " + order + ", but got: " + meal)
+		return false
+	else:
+		print("Correct meal provided! Customer is satisfied.")
+		leave()  # Assuming the customer leaves after receiving the correct meal
+		return true
