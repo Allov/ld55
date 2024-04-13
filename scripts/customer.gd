c862155b3ec8b53b79b7a6259a74f6d4846d46d6
Gabriel B.F <galabyca@gmail.com>
2024-04-13 19:44:21 -0400

    Customer version 1
    Patience bar, bubble, spawn, despawn

--- scripts/customer.gd
+++ scripts/customer.gd
@@ -1,21 +1,26 @@
-# Customer.gd
 extends Area2D
 
 # Member variables
 var order = null
-var patience = 100.0
+var initial_patience = 10.0  # Initial waiting time in seconds
+@onready var patience_timer = $PatienceTimer
+@onready var patience_bar = $PatienceBar
 
 func _ready():
-	pass
+	patience_timer.wait_time = initial_patience
+	patience_timer.start()
 
-func decrease_patience(delta):
-	patience -= delta
-	if patience <= 0:
-		leave()
+func _process(delta):
+	# Update the patience bar based on the remaining time
+	var time_left = patience_timer.time_left
+	var patience_level = time_left / initial_patience
+	patience_bar.value = patience_level
 
 func leave():
 	print("Customer is leaving due to impatience or order fulfillment.")
 	queue_free()  # This will remove the Customer instance from the scene
+	# Notify the DiningRoom to remove this customer from the list
+	get_parent().remove_customer(self)
 
 func set_order(new_order):
 	order = new_order
@@ -33,3 +38,8 @@ func receive_order(meal):
 		print("Correct meal provided! Customer is satisfied.")
 		leave()  # Assuming the customer leaves after receiving the correct meal
 		return true
+
+
+func _on_patience_timer_timeout():
+	GameManager.lose_life();
+	leave()
--- scripts/diningroom.gd
+++ scripts/diningroom.gd
@@ -1,21 +1,39 @@
-# diningroom.gd
-extends Node
+extends Node2D
 
 var customers = []
 var current_orders = {}
 var customer_spawn_timer
+var customer_positions = [Vector2(52, 170), Vector2(82, 170), Vector2(102, 170)]
 var customer = preload("res://scenes/customer.tscn") 
 
 func _ready():
+	spawn_customer()
 	customer_spawn_timer = $Customer_spawn_timer 
 	customer_spawn_timer.start()
 
 func _on_customer_spawn_timer_timeout():
-	spawn_customer()
+	if len(customers) < 3:
+		spawn_customer()
+	else:
+		customer_spawn_timer.stop()
+  
+func get_customers():
+	return customers
 
 func spawn_customer():
 	var new_customer = customer.instantiate()
-	new_customer.set_order("default")  # Set a default order, can be randomized later
+	var position_index = len(customers) % customer_positions.size()
+	new_customer.position = customer_positions[position_index]
+	new_customer.set_order("default") #Commande du client à default, todo: rendre random
 	add_child(new_customer)
 	customers.append(new_customer)
 	print("New customer spawned with order: " + new_customer.order)
+
+func _process(delta):
+	pass
+
+func remove_customer(customer):
+	if customer in customers:
+		customers.erase(customer)
+		if len(customers) < 3 and not customer_spawn_timer.is_stopped():
+			customer_spawn_timer.start()
