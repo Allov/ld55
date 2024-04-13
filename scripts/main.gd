4e7161988fc12e93d4acf73b5d19fd998153e8e5
= <=>
2024-04-12 23:56:58 -0400

    scene transitions

---
+++ scripts/main.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Start.connect("pressed", _on_Start_pressed)
+
+func _on_Start_pressed():
+	get_tree().change_scene_to_file("res://scenes/restaurant.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
