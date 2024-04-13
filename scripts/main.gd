b6d8513538915eb430120e723da92bebda47eb95
= <=>
2024-04-13 07:26:45 -0400

    main button grab focus

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Start.connect("pressed", _on_Start_pressed)
+	$Start.grab_focus()
 
 func _on_Start_pressed():
 	get_tree().change_scene_to_file("res://scenes/restaurant.tscn")
