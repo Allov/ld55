ed7f71ee19815e94f7c383f8cd696ab8055e01c0
Wenzav <aubincarl@hotmail.com>
2024-04-14 09:37:54 -0400

    Merge + Fix

--- components/hitbox.gd
+++ components/hitbox.gd
@@ -1,11 +1,10 @@
 class_name Hitbox
 extends Area2D
 
-@export_range(1, 10) var damage: int = 1 : set = set_damage, get = get_damage
+@export var damage: int = 10 : set = set_damage, get = get_damage
 
 func set_damage(value: int):
-	var clamped_value = clamp(value, 1, 10)
-	damage = clamped_value
+	damage = value
 
 func get_damage():
 	return damage
